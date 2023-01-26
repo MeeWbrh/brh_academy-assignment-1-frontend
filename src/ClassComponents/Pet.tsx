10f7231e4a62d25f741e834bb87e6919ddd74b08
MeeWbrh <bawar.hamid@meew.dk>
2023-01-26 15:50:36 +0100

    Pet Component fixet

--- src/ClassComponents/Pet.tsx
+++ src/ClassComponents/Pet.tsx
@@ -1,4 +1,5 @@
 import { IonList, IonItem, IonLabel } from '@ionic/react';
+import { FC } from 'react';
 
 type PetProps = {
   animalType: string;
@@ -8,24 +9,22 @@ type PetProps = {
   owner: string;
 };
 
-export function Pet({ animalType, breed, name, age, owner }: PetProps) {
-  return (
-    <IonList>
-      <IonItem>
-        <IonLabel>Animal: {animalType}</IonLabel>
-      </IonItem>
-      <IonItem>
-        <IonLabel>Breed: {breed}</IonLabel>
-      </IonItem>
-      <IonItem>
-        <IonLabel>Pet Name: {name}</IonLabel>
-      </IonItem>
-      <IonItem>
-        <IonLabel>Age: {age}</IonLabel>
-      </IonItem>
-      <IonItem>
-        <IonLabel>Owner: {owner}</IonLabel>
-      </IonItem>
-    </IonList>
-  );
-}
+export const Pet: FC<PetProps> = ({ animalType, breed, name, age, owner }) => (
+  <IonList>
+    <IonItem>
+      <IonLabel>Animal: {animalType}</IonLabel>
+    </IonItem>
+    <IonItem>
+      <IonLabel>Breed: {breed}</IonLabel>
+    </IonItem>
+    <IonItem>
+      <IonLabel>Pet Name: {name}</IonLabel>
+    </IonItem>
+    <IonItem>
+      <IonLabel>Age: {age}</IonLabel>
+    </IonItem>
+    <IonItem>
+      <IonLabel>Owner: {owner}</IonLabel>
+    </IonItem>
+  </IonList>
+);
